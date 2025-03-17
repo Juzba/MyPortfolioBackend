e63881b8cc77dcd2ec297a4c2b323000410650ef
Jiří Strnadel <Juzba88@gmail.com>
2025-03-17 02:55:31 +0100

    Přidejte soubory projektu.

---
+++ MyPortfolioBackend/Controllers/EmailController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolioBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly EmailService _emailService;
+
+        public EmailController()
+        {
+            _emailService = new EmailService();
+        }
+
+        [HttpPost]
+        [Route("send")]
+        public IActionResult SendEmail([FromBody] EmailRequest emailRequest)
+        {
+            _emailService.SendEmail(emailRequest.To, emailRequest.Subject, emailRequest.Message);
+            return Ok(new { message = "Email sent successfully!" });
+        }
+    }
+
+    public class EmailRequest
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
+
---
+++ MyPortfolioBackend/EmailService.cs
@@ -0,0 +1,32 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+
+
+namespace MyPortfolioBackend;
+
+public class EmailService
+{
+    private readonly string _smtpServer = "smtp.gmail.com"; // Změňte na váš SMTP server  
+    private readonly int _port = 587; // Typický port pro SMTP  
+    private readonly string _username = "Juzba88@gmail.com"; // Váš e-mail  
+    private readonly string _password = "your-email-password"; // Vaše heslo  
+
+    public void SendEmail(string toEmail, string subject, string message)
+    {
+        var emailMessage = new MimeMessage();
+        emailMessage.From.Add(new MailboxAddress("Your Name", _username));
+        emailMessage.To.Add(new MailboxAddress("Blabla",toEmail));
+        emailMessage.Subject = subject;
+        emailMessage.Body = new TextPart("html") { Text = message };
+
+        using (var client = new SmtpClient())
+        {
+            client.Connect(_smtpServer, _port, true);
+            client.Authenticate(_username, _password);
+            client.Send(emailMessage);
+            client.Disconnect(true);
+        }
+    }
+}
+
+
