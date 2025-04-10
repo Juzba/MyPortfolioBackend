aa1104675947443d0a14f9e69c4b84584c69791f
Jiří Strnadel <Juzba88@gmail.com>
2025-04-10 14:16:02 +0200

    add online checek

---
+++ MyPortfolioBackend/Controllers/OnlineController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolioBackend.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OnlineController : Controller
+    {
+
+
+        [HttpGet]
+
+
+        public IActionResult Get()
+        {
+            return Ok("Server jede ultra OK!");
+        }
+    }
+}
